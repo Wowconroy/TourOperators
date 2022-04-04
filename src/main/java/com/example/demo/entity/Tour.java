e15458bdeb912124b41391246cd8764a805640c1
Oleksii <shostak.ok@gmail.com>
2022-04-04 13:16:26 +0300

    Update Tour

--- src/main/java/com/example/demo/entity/Tour.java
+++ src/main/java/com/example/demo/entity/Tour.java
@@ -12,8 +12,47 @@ public class Tour {
     @Column(name = "tour_name")
     private String name;
 
+    @Enumerated(EnumType.STRING)
     private TourType tourType;
 
+    public Tour() {
+    }
 
+    public Tour(String name, TourType tourType) {
+        this.name = name;
+        this.tourType = tourType;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TourType getTourType() {
+        return tourType;
+    }
+
+    public void setTourType(TourType tourType) {
+        this.tourType = tourType;
+    }
+
+    @Override
+    public String toString() {
+        return "Tour{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tourType=" + tourType +
+                '}';
+    }
 }
